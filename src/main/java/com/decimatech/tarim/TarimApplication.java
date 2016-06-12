11c4245074ae4e912634ba0663857ceeb01020dc
Fatih Erdem <93.fatiherdem@gmail.com>
2016-06-13 02:14:38 +0300

    HTML icindeki enum selectler duzeltildi

--- src/main/java/com/decimatech/tarim/TarimApplication.java
+++ src/main/java/com/decimatech/tarim/TarimApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpEncodingProperties;
 import org.springframework.boot.context.web.OrderedCharacterEncodingFilter;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableCaching
 public class TarimApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/decimatech/tarim/repository/CityRepository.java
+++ src/main/java/com/decimatech/tarim/repository/CityRepository.java
@@ -1,12 +1,16 @@
 package com.decimatech.tarim.repository;
 
 import com.decimatech.tarim.model.entity.City;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
 
 public interface CityRepository extends Repository<City, Long> {
 
+    @Cacheable("cities")
     List<City> findAllByOrderByCityNameAsc();
+
+    @Cacheable("city")
     City findByCityId(Long id);
 }
