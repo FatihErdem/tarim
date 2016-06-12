cf3ca4807a7327f5f501f1e657b27f46403678c1
Fatih Erdem <93.fatiherdem@gmail.com>
2016-06-13 02:45:50 +0300

    City District ve Vendor entityleri cachelendi

--- src/main/java/com/decimatech/tarim/repository/DistrictRepository.java
+++ src/main/java/com/decimatech/tarim/repository/DistrictRepository.java
@@ -1,13 +1,16 @@
 package com.decimatech.tarim.repository;
 
 import com.decimatech.tarim.model.entity.District;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
 
 public interface DistrictRepository extends Repository<District, Long> {
 
+    @Cacheable("districts")
     List<District> findByCityIdOrderByDistrictNameAsc(Long id);
 
+    @Cacheable("district")
     District findByDistrictId(Long id);
 }
--- src/main/java/com/decimatech/tarim/repository/VendorRepository.java
+++ src/main/java/com/decimatech/tarim/repository/VendorRepository.java
@@ -1,17 +1,23 @@
 package com.decimatech.tarim.repository;
 
 import com.decimatech.tarim.model.entity.Vendor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
 
 public interface VendorRepository extends Repository<Vendor, Long> {
 
+    @Cacheable("vendorByUsername")
     Vendor getVendorByUsername(String username);
 
+    @CacheEvict(value = {"vendorByUsername", "vendor", "vendors"}, allEntries = true)
     Vendor save(Vendor vendor);
 
+    @Cacheable("vendor")
     Vendor findOne(Long id);
 
+    @Cacheable("vendors")
     List<Vendor> findAll();
 }
