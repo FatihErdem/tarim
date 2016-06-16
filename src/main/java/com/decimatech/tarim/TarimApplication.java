9183a33eace7d4e18908118d9ab7d03ef0e385ff
Fatih Erdem <93.fatiherdem@gmail.com>
2016-06-16 20:38:50 +0300

    FancyBox ve MultiPart temeli atildi

--- src/main/java/com/decimatech/tarim/TarimApplication.java
+++ src/main/java/com/decimatech/tarim/TarimApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpEncodingProperties;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
 import org.springframework.boot.context.web.OrderedCharacterEncodingFilter;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.MessageSource;
@@ -11,6 +12,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.Ordered;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -50,4 +54,11 @@ public class TarimApplication {
         return filter;
     }
 
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("128MB");
+        factory.setMaxRequestSize("128MB");
+        return factory.createMultipartConfig();
+    }
 }
